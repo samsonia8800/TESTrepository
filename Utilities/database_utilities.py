97c1cbce53fcad113c4ba3998c4ff4755d0814e6
samsonia8800 <samsonia8800@yahoo.com>
2020-11-11 21:42:20 -0500

    Test

---
+++ Utilities/database_utilities.py
@@ -0,0 +1,7 @@
+def create_database(database_path: str):
+    #todo: generate the database
+    pass
+
+def save_words_to_database(database_path: str, words_list: list):
+    #todo: save wrds to db
+    pass
---
+++ Utilities/url_utilities.py
@@ -0,0 +1,12 @@
+def load_urls_from_file(file_path: str):
+    #Todo: add the code need to read the text file with the urls in it
+    pass
+
+def load_page(url: str):
+    #todo: add code that reads the url
+    pass
+
+def scrape_page(page_contents: srt):
+    #todo: analyze the text
+    pass
+
